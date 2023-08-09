a7762ad8a88e3722b9c9efbf0b324663b0679d66
CODe5753 <code.57x53@gmail.com>
2023-08-09 15:28:48 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/kotlingateway/KotlinGatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlingateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinGatewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinGatewayApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlingateway/KotlinGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlingateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinGatewayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
