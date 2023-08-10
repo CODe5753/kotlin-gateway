e9030443495651ca85519d060144eeead72fde33
CODe1995 <code.57x53@gmail.com>
2023-08-11 01:30:12 +0900

    yml 설정으로 변경

--- src/main/kotlin/com/example/kotlingateway/config/FilterConfig.kt
+++ src/main/kotlin/com/example/kotlingateway/config/FilterConfig.kt
@@ -5,14 +5,14 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-@Configuration
+//@Configuration
 class FilterConfig {
 
-    @Bean
+//    @Bean
     fun gatewayRoutes(builder: RouteLocatorBuilder): RouteLocator {
         return builder.routes()
             .route { r ->
-                r.path("first-service/**")
+                r.path("/first-service/**")
                     .filters { f ->
                         f.addRequestHeader("first-request", "first-request-header")
                             .addResponseHeader("first-response", "first-response-header")
@@ -20,7 +20,7 @@ class FilterConfig {
                     .uri("http://localhost:8081/")
             }
             .route { r ->
-                r.path("second-service/**")
+                r.path("/second-service/**")
                     .filters { f ->
                         f.addRequestHeader("second-request", "second-request-header")
                             .addResponseHeader("second-response", "second-response-header")
